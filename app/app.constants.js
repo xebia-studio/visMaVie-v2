d0bf8078dd73a4098669118354356043223ef026
Bastien Charès <charesbast@gmail.com>
2016-03-02 16:45:16 +0100

    Added the website pages as a constant and links on the home page

--- app/app.constants.js
+++ app/app.constants.js
@@ -7,3 +7,13 @@ export const alliance = [
   {name: 'Cellenza', website: 'http://www.cellenza.com/'},
   {name: 'Akamis', website: 'http://www.akamis.com/'}
 ];
+
+export const websitePages = [
+  {name: 'Nos valeurs', state: 'values', order: 1},
+  {name: 'Nos passions', state: 'passions', order: 2},
+  {name: 'Nos créations', state: 'creations', order: 3},
+  {name: 'Accélérez votre trajectoire', state: 'boostYourCareer', order: 4},
+  {name: 'Vos débuts', state: 'yourDebut', order: 5},
+  {name: 'Xebia comme si vous y étiez', state: 'visit3D', order: 6},
+  {name: 'Nous rejoindre', state: 'join', order: 7}
+];
--- app/pages/home/home.ctrl.js
+++ app/pages/home/home.ctrl.js
@@ -1,14 +1,14 @@
+import {websitePages} from '../../app.constants';
+
 export default class HomeController {
   constructor($uibModal, ExampleSvc){
     this._$uibModal = $uibModal;
     this.pageName = "Vis-ma-vie";
-    this.aVariable = "Initial value";
     this.names = ExampleSvc.getNames();
+    this.websitePages = websitePages;
   }
 
   openModal(){
-    this.aVariable = 'New value';
-
     var modalInstance = this._$uibModal.open({
       animation: true,
       template: '<div class="modal-header">Titre Modale</div>' +
