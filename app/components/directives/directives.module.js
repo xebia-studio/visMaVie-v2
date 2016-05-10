cce6ec858dfe9517ab4330c0e5c3f06b7a7c3a1a
Bastien Charès <charesbast@gmail.com>
2016-05-10 18:05:19 +0200

    Modified the css

--- app/components/directives/directives.module.js
+++ app/components/directives/directives.module.js
@@ -2,6 +2,7 @@ import xebiaUI from '../../../xebiaUI/xebiaUI.mdl';
 import XebiaHeader from './XebiaHeader/XebiaHeader.mdl';
 import XebiaFooter from './XebiaFooter/XebiaFooter.mdl';
 import CardLink from './CardLink/CardLink.mdl';
+import Showcase from './Showcase/Showcase.mdl';
 
 // This module includes all the app directives
 export default angular
@@ -9,6 +10,7 @@ export default angular
     xebiaUI,
     XebiaHeader,
     XebiaFooter,
-    CardLink
+    CardLink,
+    Showcase
   ])
   .name;
