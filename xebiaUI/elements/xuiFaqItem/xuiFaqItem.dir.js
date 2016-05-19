4daad968a7060cfbb1d66df616836f4787e80e00
Thomas Simonnet <tsimonnet@xebia.fr>
2016-05-19 09:19:10 +0200

    Added xuiFaq and xuiFaqItem components

---
+++ xebiaUI/elements/xuiFaqItem/xuiFaqItem.dir.js
@@ -0,0 +1,11 @@
+import './xuiFaqItem.less';
+
+let xuiFaqItem = {
+  template: require('./xuiFaqItem.tpl.html'),
+  bindings: {
+    question: '<',
+    answersTemplates: '<'
+  }
+};
+
+export default xuiFaqItem;
