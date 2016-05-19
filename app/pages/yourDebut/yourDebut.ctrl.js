a95d11233f9590238ce06e96178e6b98c2a1960b
Thomas Simonnet <tsimonnet@xebia.fr>
2016-05-19 09:21:13 +0200

    Updated yourDebut controller to use xuiFaq component

--- app/pages/yourDebut/yourDebut.ctrl.js
+++ app/pages/yourDebut/yourDebut.ctrl.js
@@ -1,6 +1,24 @@
+import choixMissionsTemplateUrl from '!!file!./answersTemplates/choixMissions.tpl.html';
+import dureeMissionsTemplateUrl from '!!file!./answersTemplates/dureeMission.tpl.html';
+import missionsClientTemplateUrl from '!!file!./answersTemplates/missionsClient.tpl.html';
+
 export default class YourDebutCtrl {
   constructor() {
-
+    this.faqItems = [
+        {
+            question:`Quelle possibilité d'évolution ?`,
+            answersTemplates: [
+                choixMissionsTemplateUrl,
+                dureeMissionsTemplateUrl,
+                missionsClientTemplateUrl
+            ]
+        },
+        {
+            question:`Comment se passent les missions ?`,
+            answersTemplates: [
+            ]
+        }
+    ]
   }
 
 }
