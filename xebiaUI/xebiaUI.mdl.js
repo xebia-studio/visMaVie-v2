eebadad49e43e9fb30347883ed0619402619fa50
Thomas Simonnet <tsimonnet@xebia.fr>
2016-05-19 09:20:11 +0200

    Added xuiFaq component to xebiaUI models

--- xebiaUI/xebiaUI.mdl.js
+++ xebiaUI/xebiaUI.mdl.js
@@ -2,6 +2,8 @@ import './styles/main.less';
 
 //Elements
 import xuiButton from './elements/xuiButton/xuiButton.mdl';
+import xuiFaq from './elements/xuiFaq/xuiFaq.mdl';
+import xuiFaqItem from './elements/xuiFaqItem/xuiFaqItem.mdl';
 import xuiShowcase from './elements/xuiShowcase/xuiShowcase.mdl';
 import xuiCard from './elements/xuiCard/xuiCard.mdl';
 
@@ -9,6 +11,8 @@ export default angular
   .module('xebiaUI', [
     xuiButton,
     xuiShowcase,
-    xuiCard
+    xuiCard,
+    xuiFaq,
+    xuiFaqItem
   ])
   .name;
