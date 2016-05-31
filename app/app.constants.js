10ce00c1927c682a3ee94975166ab99d3184909f
Bastien Charès <charesbast@gmail.com>
2016-05-31 23:26:48 +0200

    home: design improvements

--- app/app.constants.js
+++ app/app.constants.js
@@ -10,8 +10,23 @@ export const alliance = [
 
 export const homeXebians = [
   {firstName: 'Pablo', lastName: 'Lopez', job: 'CTO chez Xebia', imageUrl: '/assets/images/home/pablo-lopez.png'},
-  {firstName: 'Julien', lastName: 'Smadja', job: 'Manager chez Xebia', imageUrl: '/assets/images/home/julien-smadja.png'},
-  {firstName: 'Benjamin', lastName: 'Moitié', job: 'Coach Agile chez Xebia', imageUrl: '/assets/images/home/benjamin-moitie.png'},
-  {firstName: 'Philippe', lastName: 'Antoine', job: 'Scrum Master chez Xebia', imageUrl: '/assets/images/home/philippe-antoine.png'},
-  {firstName: 'Hugo', lastName: 'Geissmann', job: 'Président chez Thiga', imageUrl: '/assets/images/home/hugo-geissmann.png'}
+  // {firstName: 'Julien', lastName: 'Smadja', job: 'Manager chez Xebia', imageUrl: '/assets/images/home/julien-smadja.png'},
+  // {firstName: 'Benjamin', lastName: 'Moitié', job: 'Coach Agile chez Xebia', imageUrl: '/assets/images/home/benjamin-moitie.png'},
+  // {firstName: 'Philippe', lastName: 'Antoine', job: 'Scrum Master chez Xebia', imageUrl: '/assets/images/home/philippe-antoine.png'},
+  // {firstName: 'Hugo', lastName: 'Geissmann', job: 'Président chez Thiga', imageUrl: '/assets/images/home/hugo-geissmann.png'}
+];
+
+export const numbersCards = [
+  {icon: 'assets/icons/icon-brain-vert.svg', background: '', title: '5%', content: 'du CA dédié aux XKE'},
+  {icon: 'assets/icons/icon-people-vert.svg', background: '', title: '125', content: 'Consultants en France'},
+  {icon: 'assets/icons/icon-pen-vert.svg', background: '', title: '1800', content: 'Articles de blog'},
+  {icon: 'assets/icons/icon-world-vert.svg', background: '', title: '15', content: 'Ans'},
+  {icon: 'assets/icons/icon-birthday-cake-vert.svg', background: '', title: '45', content: 'Millions de CA'},
+  {icon: 'assets/icons/icon-evolution-vert.svg', background: '', title: '3', content: 'Pays'}
+];
+
+export const citations = [
+  'La grande majorité des collaborateurs partagent un but commun: L\'Excellence technique',
+  'Seconde citation',
+  'troisième exemple de citation'
 ];
--- app/pages/home/home.ctrl.js
+++ app/pages/home/home.ctrl.js
@@ -1,51 +1,14 @@
-import {homeXebians} from '../../app.constants';
+import {homeXebians, numbersCards, citations} from '../../app.constants';
 
 export default class HomeController {
   constructor(){
     this.xebians = homeXebians;
+    this.numbersCards = numbersCards;
+    this.citations = citations;
 
     this.showCaseTitle = 'Vis ma vie de consultant chez Xebia';
     this.showCaseMainDescription = 'Explorez notre univers et nos expertises technologiques';
     this.showCaseSubDescription = '#BeBrave #BeaXebian';
-
-    this.numbersCards = [
-      {
-        icon: 'assets/icons/icon-brain-vert.svg',
-        background: '',
-        title: '5%',
-        content: 'du CA dédié aux XKE'
-      },
-      {
-        icon: 'assets/icons/icon-people-vert.svg',
-        background: '',
-        title: '125',
-        content: 'Consultants en France'
-      },
-      {
-        icon: 'assets/icons/icon-pen-vert.svg',
-        background: '',
-        title: '1800',
-        content: 'Articles de blog'
-      },
-      {
-        icon: 'assets/icons/icon-world-vert.svg',
-        background: '',
-        title: '15',
-        content: 'Ans'
-      },
-      {
-        icon: 'assets/icons/icon-birthday-cake-vert.svg',
-        background: '',
-        title: '45',
-        content: 'Millions de CA'
-      },
-      {
-        icon: 'assets/icons/icon-evolution-vert.svg',
-        background: '',
-        title: '3',
-        content: 'Pays'
-      }
-    ];
   }
 }
 
