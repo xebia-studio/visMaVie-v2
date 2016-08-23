643121e43825d1d6d74ea6c6e5e2273695325ab3
Thomas Simonnet <tsimonnet@xebia.fr>
2016-08-23 16:12:28 +0200

    Added boostYourCareer showCase image + Fixed some showCase images glitches for yourDebut and boostYourCareer pages

--- app/pages/boostYourCareer/boostYourCareer.ctrl.js
+++ app/pages/boostYourCareer/boostYourCareer.ctrl.js
@@ -3,6 +3,10 @@ import {softSkillsCards} from '../../constants/career.constants';
 export default class BoostYourCareerCtrl {
   constructor(){
     this.softSkillsCards = softSkillsCards;
+
+    this.showCaseTitle = 'Accélérez votre carrière';
+    this.showCaseMainDescription = "Choisissez votre route, tout est possible, rien n'est écrit.";
+    this.showCaseSubDescription = '';
   }
 }
 
