f0bfead591bd476ccaa2ec960d3e5a9656e74320
Thomas Simonnet <tsimonnet@xebia.fr>
2016-12-22 19:01:44 +0100

    Add Tester component to create and test components + Add StatCard component

---
+++ src/main/components/StatCard.vue
@@ -0,0 +1,64 @@
+<template lang="jade">
+    .StatCard
+        .StatCard-picto-wrapper
+            .StatCard-picto(:is='picto')
+        .StatCard-stat-wrapper
+            .StatCard-stat {{stat}}
+            .StatCard-text {{text}}
+
+</template>
+
+<script>
+    export default {
+        name: 'StatCard',
+        props: {
+            picto: {
+                required: true
+            },
+            stat: {
+                type: String,
+                required: true
+            },
+            text: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+
+<style lang="stylus">
+    _pictoColor = color__$orange
+    _statColor = color__$blue
+    _textColor = color__$blue
+
+    _statFontSize = fontsize__$sectionTitle
+    _textFontSize = fontsize__$mainFontSize
+
+    .StatCard
+        width 80px
+        height 100px
+        background-color white
+
+    .StatCard-picto-wrapper
+        height (100*2/3) %
+
+    .StatCard-picto
+        margin-left auto
+        margin-right auto
+        width 60px
+        height 100%
+        color _pictoColor
+
+    .StatCard-stat-wrapper
+        height (100*1/3) %
+
+    .Statcard-stat
+        color _statColor
+        font-size _statFontSize
+
+    .StatCard-text
+        color _textColor
+        font-size _textFontSize
+
+</style>
---
+++ src/main/components/Tester.vue
@@ -0,0 +1,30 @@
+<template lang="jade">
+    StatCard(:picto="statData.picto", :stat="statData.stat", :text="statData.text")
+</template>
+
+<script>
+import StatCard from 'components/StatCard'
+import LogoXebia from 'src/generated/components/LogoXebia'
+
+export default {
+	name: 'Tester',
+	data: function () {
+		return {
+			statData: {
+				picto: LogoXebia,
+				stat: '3',
+				text: 'Toto'
+			}
+		}
+	},
+	components: {
+		StatCard,
+		LogoXebia
+	}
+}
+</script>
+
+<style lang="stylus">
+    body
+        background-color black
+</style>
