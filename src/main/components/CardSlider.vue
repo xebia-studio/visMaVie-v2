871d3417ae33b819cbc342ff6f119a8253cc1204
Thomas Simonnet <tsimonnet@xebia.fr>
2017-02-14 13:18:15 +0100

    #19 - Finish CardSlider Desktop Version

--- src/main/components/CardSlider.vue
+++ src/main/components/CardSlider.vue
@@ -1,15 +1,22 @@
 <template lang="jade">
-    .CardSlider
-        Slider.CardSlider-slider(:breakpoints='sliderBreakpoints', ref='slider', @slideCountChange='updateSlideCount', loop=true, :speed='800', :spaceBetween='20', pagination='.CardSlider-pagination', :paginationBulletRender='paginationBulletRender')
-            SliderSlide.CardSlider-slide(v-for="card in cards", ref="card")
-                .CardSlider-slide-inner-wrapper
-                    .Card-image(:is="svgComponent(card.image)")
-                    .Card-title(v-html="card.title")
-                    .Card-text(v-html="card.text")
-        .CardSlider-pagination(ref='pagination', :style="{width: '100%'}")
+    .CardSlider(v-bind:style="{height: isWidthCompact ? '' : componentDesktopHeight}")
+        .CardSlider-wrapper
+            Slider.CardSlider-slider(:breakpoints='sliderBreakpoints', ref='slider', loop=false, :speed='400', pagination='.CardSlider-pagination', :paginationBulletRender='paginationBulletRender')
+                SliderSlide.CardSlider-slide(v-for="card, index in cards", ref="card", v-bind:style="cardsDesktopPosition[index].css")
+                    .CardSlider-slide-inner-wrapper(v-bind:style="maxCardHeight[index]")
+                        .Card-image(:is="svgComponent(card.image)")
+                        .Card-title(v-html="card.title")
+                        .Card-text(v-html="card.text", v-if="!isWidthCompact")
+                        a.Card-link(v-bind:href="card.link", target="_blank") En Savoir Plus
+        .CardSlider-pagination(ref='pagination', :style="{width: paginationWidth+ 'px'}")
 </template>
 
 <script>
+import { range } from 'lodash';
+import { nextTick } from 'vue';
+import { domHeight } from '@alexistessier/dom';
+
+import { mixin as fontLoader } from 'tools/font-loader';
 import { mixin as svgComponent } from 'tools/svg-component';
 import { mixin as sizeClassHelper, config as sizeClassHelperConfig } from 'tools/size-class-helper';
 
@@ -23,7 +30,7 @@ const scrollBarWidth = getScrollBarWidth();
 
 export default {
 	name: 'CardSlider',
-	mixins: [svgComponent, sizeClassHelper],
+	mixins: [fontLoader, svgComponent, sizeClassHelper],
 	props: {
 	    cards: {
 	        required: true
@@ -31,55 +38,112 @@ export default {
 	},
 	data: function () {
 	    return {
-	        slideCount: 0
+	        isWidthCompact: this.getSizeClassHelper().isActive('width-compact'),
+	        maxCardHeight: this.cards.map(() => {return {height: 'auto'};}),
+	        cardsDesktopPosition: this.cards.map(() => {return {css: {top: '0px', left: '0px'}, rawTop: 0};}),
+	        componentDesktopHeight: 0
 	    };
 	},
 	computed: {
 	    paginationWidth: function () {
-			const paginationBulletSize = settings.paginationBulletSize + settings.paginationBulletBorderWidth;
-			const countBullet = this.slideCount;
-
-			return (countBullet * paginationBulletSize + Math.max(0, countBullet - 1) * settings.paginationBulletsSpaceBetween);
+			const paginationBulletSize = settings.paginationBulletSize + (settings.paginationBulletBorderWidth*2);
+			return (this.cards.length * paginationBulletSize + Math.max(0, this.cards.length - 1) * settings.paginationBulletsSpaceBetween);
 		},
 	    sliderBreakpoints: function () {
 			const conf = {};
 			conf[sizeClassHelperConfig['width-compact'].maxWidth+scrollBarWidth] = {
-				slidesPerView: settings.sizeClassCompactSlidesPerView
+				slidesPerView: 1,
+				spaceBetween: 20
 			};
 
 			//Infinity
 			//Must be defined, otherwise the pagination is bugged when switching from one breakpoints to an other
 			conf[Infinity] = {
-				slidesPerView: 1
+				slidesPerView: 1,
+				spaceBetween: 0
 			};
 			return conf;
 		}
 	},
 	methods: {
-	    updateSlideCount: function (count) {
-			this.slideCount = count;
-		},
 		paginationBulletRender: function (swiper, index, className) {
 			return '<button type="button" class="CardSlider-pagination-bullet ' + className + '">' + (index + 1) + '</button>';
 		},
 		updateLayoutOnDeviceChange: function () {
 			this.$refs.slider ? this.$refs.slider.update(true) : null;
-		}
+		},
+		updateLayoutOnResize: function () {
+		    this.maxCardHeight = this.cards.map(() => {return {height: 'auto'};});
+            nextTick(() => {
+                if(this.$refs.card) {
+                    if(this.isWidthCompact) {
+                        const maxCardHeight = Math.max(...this.$refs.card.map((cardItem) => domHeight(cardItem.$el)));
+                        this.maxCardHeight = this.cards.map(() => {return {height: (maxCardHeight-(settings.cardBorder*2))+'px'};});
+                    } else {
+                        const columnHeights = range(settings.desktopCardsPerRow).map(() => 0);
+                        const temporaryCardsDesktopPosition = this.cards.map(() => {return {css: {top: '0px', left: '0px'}, rawTop: 0};});
+                        const cardHeights = this.$refs.card.map((card) => {
+                            return domHeight(card.$el);
+                        });
+                        this.cards.forEach((card, index) => {
+                            // Get the column where you have to place the card
+                            let targetColumnIndex = -1;
+                            if(index >= parseInt(this.cards.length/settings.desktopCardsPerRow)*settings.desktopCardsPerRow && this.cards.length%settings.desktopCardsPerRow != 0) {
+                                targetColumnIndex = columnHeights.indexOf(Math.min(...columnHeights));
+                            } else {
+                                targetColumnIndex = index%settings.desktopCardsPerRow;
+                            }
+                            temporaryCardsDesktopPosition[index].css.left = 1/settings.desktopCardsPerRow*100*(targetColumnIndex)+'%';
+                            if(index >= settings.desktopCardsPerRow) {
+                                const targetColumnLastCardIndex = ((parseInt(index/settings.desktopCardsPerRow)-1)*settings.desktopCardsPerRow) + targetColumnIndex;
+                                const targetColumnLastCardBottom =  temporaryCardsDesktopPosition[targetColumnLastCardIndex].rawTop + cardHeights[targetColumnLastCardIndex];
+                                temporaryCardsDesktopPosition[index].css.top = 'calc('+ targetColumnLastCardBottom + 'px + ' + parseInt(index/settings.desktopCardsPerRow)*30 + 'px)';
+                                temporaryCardsDesktopPosition[index].rawTop = targetColumnLastCardBottom;
+                            }
+                            columnHeights[targetColumnIndex] += cardHeights[index];
+                        });
+                        this.componentDesktopHeight = (Math.max(...columnHeights)+parseInt(this.cards.length/settings.desktopCardsPerRow)*30)+'px';
+                        this.cardsDesktopPosition = temporaryCardsDesktopPosition;
+                    }
+                }
+            });
+        }
 	},
 	created: function () {
+	    this.loadFont({
+            'text': [],
+            'title': ['light', 'regular']
+            },
+            {
+            fontactive: () => {
+                this.activeFont = this.activeFont ? (this.activeFont+1) : 1;
+                if(this.activeFont >= 2){
+                    this.updateLayoutOnResize();
+                }
+            }
+        });
 		const sizeClassHelper = this.getSizeClassHelper();
-
+		this.getSizeClassHelper().on('resize', () => {
+            this.isWidthCompact = this.getSizeClassHelper().isActive('width-compact');
+        });
 		const change = () => {
 			this.updateLayoutOnDeviceChange();
 		};
+		const resize = () => {
+            this.updateLayoutOnResize();
+        };
 		this.deviceChangeListenerArguments = ['change', change];
 		sizeClassHelper.on(...this.deviceChangeListenerArguments);
+		this.resizeListenerArguments = ['resize', resize];
+		sizeClassHelper.on(...this.resizeListenerArguments);
 	},
 	mounted: function () {
+	    this.updateLayoutOnResize();
 		this.updateLayoutOnDeviceChange();
 	},
 	beforeDestroy: function () {
 		this.getSizeClassHelper().off(...this.deviceChangeListenerArguments);
+		this.getSizeClassHelper().off(...this.resizeListenerArguments);
 	},
 	components: {
 	    Slider,
@@ -95,32 +159,63 @@ export default {
     _paginationBulletBorderWidth = CardSlider__$paginationBulletBorderWidth px
     _paginationBulletsSpaceBetween = CardSlider__$paginationBulletsSpaceBetween px
 
-    .CardSlider
-        width 100%
-        position relative
+    .CardSlider-wrapper
         .size-class-width-compact &
-            overflow hidden
-            _width = (100% * CardSlider__$sizeClassCompactSlidesPerView)
-            width _width
-            right ( (_width - 100) / 2 )
+            width 80%
+            margin-left auto
+            margin-right auto
 
     .CardSlider-slider
-        width 100%
-        &:after
-            content ''
-            display block
-            clear both
         .size-class-width-compact &
             width (560 / 640 * 100%)
             margin-right auto
             margin-left auto
+            &:after
+                content ''
+                display block
+                clear both
+        .size-class-not-width-compact &
+            display flex
+            flex-direction column
+
+    .Slider-slides-list
+        .size-class-not-width-compact &
+            transform translate3d(0px, 0px, 0px) !important
+            position relative
+            width 100% !important
+            &:hover
+                cursor auto
+
 
     .CardSlider-slide
-        width 100%
-        float left
+        .size-class-width-compact &
+            width 100%
+            float left
+            layout__outerBox()
+        .size-class-not-width-compact &
+            position absolute
+            width (1 / 3 * 100%) !important
 
     .CardSlider-slide-inner-wrapper
-        layout__outerBox()
+        margin-left auto
+        margin-right auto
+        background-color color__$sectionOdd
+        padding 0 20px
+        width 80%
+        line-height (font__$textLineHeight)px
+        border (CardSlider__$cardBorder)px solid color__$greyLight
+
+    .Card-title
+        padding-bottom 20px
+        font__useTitleRegular 30px
+        line-height (font__$sectionTitleLineHeight)px
+        color color__$title
+
+    .Card-link
+        text-transform uppercase
+        color color__$flashOrange
+        display block
+        padding 20px 0
 
     .CardSlider-pagination
         display block
