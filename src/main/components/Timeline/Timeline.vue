8696b8660edbc81bffdb7cb092e80930bbb140c4
Bastien Charès <charesbast@gmail.com>
2017-02-15 14:26:11 +0100

    WIP

---
+++ src/main/components/Timeline/Timeline.vue
@@ -0,0 +1,23 @@
+<template lang="jade">
+  .Timeline
+    .Timeline-centralLine
+    .Timeline-item(v-for='item in items')
+      .Timeline-item-year(v-html='item.year')
+      .Timeline-item-block
+        .Timeline-item-block-image(v-html='item.image')
+        .Timeline-item-block-description(v-html='item.description')
+</template>
+
+<script>
+  export default {
+    name : 'Timeline',
+    props : {
+      items : {
+        required : true
+      }
+    },
+    created : function() {}
+  };
+</script>
+
+<style lang="stylus" src="./Timeline.styl"></style>
