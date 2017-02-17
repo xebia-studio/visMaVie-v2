9cd637bea1331c3a50c5107fd71d287a63a1444c
Thomas Simonnet <tsimonnet@xebia.fr>
2017-02-17 18:43:19 +0100

    Add title tags to each page

--- src/main/settings/routes/index.js
+++ src/main/settings/routes/index.js
@@ -12,17 +12,32 @@ const routes = concat(
 		{
 			name: 'home',
 			path: '/',
-			component: resolve => require(['components/pages/Home/Home.vue'], resolve)
+			component: resolve => require(['components/pages/Home/Home.vue'], resolve),
+      beforeEnter: (to, from, next) => {
+        const title = document.querySelector('title');
+        title.innerHTML = 'Vis Ma Vie - Xebia France';
+        next();
+      }
 		},
     {
       name : 'creations',
       path : '/creations',
-      component : resolve => require(['components/pages/NosCreations/NosCreations.vue'], resolve)
+      component : resolve => require(['components/pages/NosCreations/NosCreations.vue'], resolve),
+      beforeEnter: (to, from, next) => {
+        const title = document.querySelector('title');
+        title.innerHTML = 'Vis Ma Vie - Nos créations';
+        next();
+      }
     },
     {
       name : 'integration',
       path : '/integration',
-      component : resolve => require(['components/pages/VotreIntegration/VotreIntegration.vue'], resolve)
+      component : resolve => require(['components/pages/VotreIntegration/VotreIntegration.vue'], resolve),
+      beforeEnter: (to, from, next) => {
+        const title = document.querySelector('title');
+        title.innerHTML = 'Vis Ma Vie - Votre intégration';
+        next();
+      }
     },
     {
       name : 'nous-rejoindre',
@@ -30,6 +45,8 @@ const routes = concat(
       component : resolve => require(['components/pages/NousRejoindre/NousRejoindre.vue'], resolve),
       beforeEnter : (to, from, next) => {
         const jobParam = get(to, 'params.job');
+        const title = document.querySelector('title');
+        title.innerHTML = 'Vis Ma Vie - Nous rejoindre';
         return (isUndefined(jobParam) || isAValidJob(jobParam))
           ? next()
           : next('/nous-rejoindre/');
