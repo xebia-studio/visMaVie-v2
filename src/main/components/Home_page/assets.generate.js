da72ddfa9e3589d25eb507a2c6f053c866d18d5d
Alexis Tessier <alexistessierdev@gmail.com>
2017-07-05 18:24:16 +0200

    home header version desktop

--- src/main/components/Home_page/assets.generate.js
+++ src/main/components/Home_page/assets.generate.js
@@ -9,9 +9,13 @@ function imagePath(imageName) {
 const header = require(`data/home/header.json`);
 imagesToBlur.push(imagePath(header.image.default), imagePath(header.image.mobile));
 
+const images = header.consultants.map(consultant => imagePath(consultant.image));
+
 module.exports = {
+	images,
 	blurImages: imagesToBlur,
 	svgComponents: {
-		LogoXebiaVisMaVie: imagePath('svg/logo-xebia-vis-ma-vie.svg')
+		LogoXebiaVisMaVie: imagePath('svg/logo-xebia-vis-ma-vie.svg'),
+		ArrowBottom: imagePath('svg/arrow-bottom.svg')
 	}
 }
