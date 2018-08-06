1cc044d298eb2944ca60c7ee094db299d928c974
paque <paque.melanie@gmail.com>
2018-08-06 14:57:53 +0200

    correction font-weight footer

--- src/main/components/FooterNavigationBar/FooterNavigationBar.vue
+++ src/main/components/FooterNavigationBar/FooterNavigationBar.vue
@@ -34,39 +34,39 @@ export default {
 		overflow hidden
 		display block
 		background-color color__$blue
-	
+
 	.FooterNavigationBar-useful-width-box
 		layout__outerBox()
 
 	.FooterNavigationBar-margin-box
 		layout__innerBox()
-	
+
 	.FooterNavigationBar-links-list
 		layout__centeredGridBox(28)
 		text-align center
 		padding-top 10px
 		padding-bottom 12px
-	
+
 	.FooterNavigationBar-label,
 	.FooterNavigationBar-link
 		display inline-block
-		font__useTextThin 12
+		font__useTextRegular 12
 		font__line-height 16
 		color white
 		position relative
-	
+
 	.FooterNavigationBar-link
 		text-decoration none
-		
+
 		&:hover, &:focus
 			color color__$uiEnabled
-	
+
 	.FooterNavigationBar-link+.FooterNavigationBar-link,
 	.FooterNavigationBar-link+.FooterNavigationBar-label,
 	.FooterNavigationBar-label+.FooterNavigationBar-link,
 	.FooterNavigationBar-label+.FooterNavigationBar-label
 		margin-left 30px
-		
+
 		&:before
 			shape__circle(4px, white)
 			content ''
